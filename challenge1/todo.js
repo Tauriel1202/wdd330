259adbe3084bf2028314f0c64b41dd9fd3762f7e
Tauriel1202 <lothlorienexplorer20@gmail.com>
2022-05-21 15:35:33 -0400

    js working :)

--- challenge1/todo.js
+++ challenge1/todo.js
@@ -1,3 +1,5 @@
+console.clear();
+
 let todos = [];
 const lst = document.querySelector(".todoLst");
 const input = document.querySelector("input");
@@ -12,11 +14,13 @@ function main() {
   display();
 
   // done();
+  input.value = "";
+  input.focus();
 }
 
 //Add to list
 function add() {
-  todos.push(input.value)
+  todos.push(input.value);
   return todos;
 }
 
@@ -30,29 +34,20 @@ function display() {
 }
 
 lst.addEventListener("click", (e) => {
-  //Done
-  if (e.target.checked) {
-    let p = e.target.parentElement.nextSibling
-    p.innerHTML = `<strike>${p.innerHTML}</strike>`
+  if (e.target.tagName != "DIV") {
+    //Remove Todo
+    if (e.target.innerHTML === "❌") {
+      const index = Array.from(lst.children).indexOf(e.target.parentElement);
+      todos.pop(index);
+      e.target.parentElement.remove();
+    }
+    //Done
+    else if (e.target.checked) {
+      let p = e.target.parentElement.nextSibling;
+      p.innerHTML = `<strike>${p.innerHTML}</strike>`;
+    } else if (e.target.tagName == 'INPUT') {
+      let p = e.target.parentElement.nextSibling;
+      p.innerHTML = `${p.children[0].innerHTML}`;
+    }
   }
-  else {
-    let p = e.target.parentElement.nextSibling
-    p.innerHTML = `${p.children[0].innerHTML}`
-  }
-  //Remove Todo
-  if (e.target.innerHTML === "❌") {
-    const index = Array.from(lst.children).indexOf(e.target.parentElement);
-    todos.pop(index);
-    e.target.parentElement.remove();
-  }
-
 });
-
-
-
-
-
-// function clear(){
-//   input.value.textContent = '';
-//   return input;
-// }
